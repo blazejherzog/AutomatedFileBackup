1493340952734eb7f968a7b93231425ac911eb2e
Blazej Herzog <blazejherzog@gmail.com>
2024-01-17 20:13:02 +0100

    Creates automated file backup

---
+++ file_backup.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = "/Users/blazejherzog/Documents"
+destination_dir = "/Users/blazejherzog/Desktop/Backups"
+
+
+def copy_folder_to_directory(source, destination):
+    today = datetime.date.today()
+    dest_dir = os.path.join(destination, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {destination}")
+
+
+schedule.every().day.at("07:00").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
